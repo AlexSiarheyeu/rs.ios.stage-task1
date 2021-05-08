13738ecc68e9a7604299c2cadae158ef433757e1
Alex Sergeev <alsergeevfit@gmail.com>
2021-05-08 11:00:15 +0300

    feat: finished 4th task

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,25 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+        
+    NSMutableArray *dome = [NSMutableArray new];
+    NSString *stringValue = [number stringValue];
+    
+    unsigned int len = [stringValue length];
+    char buffer[len];
+    
+    [stringValue getCharacters:buffer range:NSMakeRange(0, len)];
+    
+    for(int i = 0; i < len; ++i) {
+       char current = buffer[i];
+        [dome addObject: [NSString stringWithFormat:@"%C", [stringValue characterAtIndex:current]]];
+    }
+    
+    for (int i = 0; i<10; i++) {
+        [dome exchangeObjectAtIndex:0 withObjectAtIndex:dome.count-1];
+    }
+    
+    return dome;
 }
 
 @end
