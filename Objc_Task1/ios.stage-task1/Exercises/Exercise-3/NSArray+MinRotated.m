a2975f8ab76b91e038fcc35be25c655176da2762
Alex Sergeev <alsergeevfit@gmail.com>
2021-05-11 13:41:24 +0300

    fix: refactoring 3rd task

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -2,8 +2,37 @@
 
 @implementation NSArray (MinRotated)
 
-- (NSNumber *)minRotated {
+// MARK: The most easiest way
+
+- (NSNumber *)minRotated_0 {
     return [self valueForKeyPath:@"@min.self"];
 }
+
+// MARK: Solution according to the requirements
+
+- (NSNumber *)minRotated {
+    
+    if ([self firstObject] == nil) {
+        return nil;
+    }
+    
+    NSInteger firstElement = 0;
+    NSInteger lastElement = self.count - 1;
+    NSNumber *last = self.lastObject;
+
+    while (firstElement < lastElement) {
+        
+        NSInteger mid = firstElement + (lastElement - firstElement) / 2;
+        NSNumber *temporaryMid = self[mid];
+        
+        if (temporaryMid.intValue > last.intValue) {
+            firstElement = mid + 1;
+        } else {
+            lastElement = mid;
+        }
+    }
+    return self[lastElement];
+}
+
 
 @end
